33f7b84acc70858055093e93ef08262cbb122294
Tim Mewe <tim.mewe@me.com>
2018-12-19 10:10:10 +0100

    basic folding cell implementation

--- Weckr/Views/Cells/RouteTableViewCell.swift
+++ Weckr/Views/Cells/RouteTableViewCell.swift
@@ -17,9 +17,8 @@ class RouteTableViewCell: FoldingCell, Reusable {
 //        gradient = (UIColor.morningRoutineCellLeft.cgColor, UIColor.morningRoutineCellRight.cgColor)
         backgroundColor = .clear
         
-        addSubviews()
-        setupConstraints()
-        commonInit()
+        backViewColor = .red
+        itemCount = 4
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -27,31 +26,68 @@ class RouteTableViewCell: FoldingCell, Reusable {
     }
     
     func configure(with route: Route) {
+        addSubviews()
+
+        let container = createContainerView(maneuvers: route.legs.first!.maneuvers.toArray())
+        contentView.addSubview(container)
+        containerView = container
+        
+        setupConstraints()
+        commonInit()
     }
     
     private func addSubviews() {
-        addSubview(foreground)
-        addSubview(container)
+        contentView.addSubview(foreground)
         foregroundView = foreground
-        containerView = container
     }
     
     private func setupConstraints() {
         let insets = Constraints.Main.Tile.self
-        foregroundViewTop = foregroundView.autoPinEdge(.top, to: .top, of: self, withOffset: insets.top)
+        
+        contentView.autoPinEdge(.left, to: .left, of: self, withOffset: insets.left)
+        contentView.autoPinEdge(.right, to: .right, of: self, withOffset: -insets.right)
+        
+        foregroundViewTop = foregroundView.autoPinEdge(.top, to: .top, of: contentView, withOffset: insets.top)
         foregroundView.autoSetDimension(.height, toSize: 100)
-        foregroundView.autoPinEdge(.left, to: .left, of: self, withOffset: insets.left)
-        foregroundView.autoPinEdge(.right, to: .right, of: self, withOffset: -insets.right)
+        foregroundView.autoPinEdge(.left, to: .left, of: contentView)
+        foregroundView.autoPinEdge(.right, to: .right, of: contentView)
         
-        containerViewTop = contentView.autoPinEdge(.top, to: .top, of: self, withOffset: 120)
+        containerViewTop = containerView.autoPinEdge(.top, to: .top, of: contentView, withOffset: 150)
+        containerView.autoPinEdge(.left, to: .left, of: contentView)
+        containerView.autoPinEdge(.right, to: .right, of: contentView)
+    }
+    
+    override func animationDuration(_ itemIndex:NSInteger, type:AnimationType) -> TimeInterval {
+        let durations = [0.3, 0.3, 0.3] // timing animation for each view
+        return durations[itemIndex]
+    }
+    
+    let rowHeight = 100
+    
+    private func createContainerView(maneuvers: [Maneuver]) -> UIView {
+        let count = 3
         
-//        let insets = Constraints.Main.Text.self
-//        infoView.autoPinEdgesToSuperviewEdges(with: UIEdgeInsets(top: insets.top,
-//                                                                 left: insets.left,
-//                                                                 bottom: insets.bottom,
-//                                                                 right: insets.right),
-//                                              excludingEdge: .bottom)
-//        tileView.autoPinEdge(.bottom, to: .bottom, of: infoView, withOffset: insets.bottom)
+        let content = UIView.newAutoLayout()
+        content.backgroundColor = .clear
+        
+        for i in 0...count-1 {
+            let view = TileView.newAutoLayout()
+            content.addSubview(view)
+
+//            let offset = i == 0 ? 10 : (i * (rowHeight + 20) + 10
+
+            view.backgroundColor = .red
+            view.autoSetDimension(.height, toSize: CGFloat(rowHeight))
+            view.autoPinEdge(.left, to: .left, of: content)
+            view.autoPinEdge(.right, to: .right, of: content)
+            view.autoPinEdge(.top, to: .top, of: content, withOffset: CGFloat((i * (rowHeight + 20))))
+            
+            if (i == count - 1) {
+                content.autoPinEdge(.bottom, to: .bottom, of: view, withOffset: 10)
+            }
+        }
+        
+        return content
     }
     
     let foreground: RotatedView = {
@@ -61,14 +97,17 @@ class RouteTableViewCell: FoldingCell, Reusable {
         view.backgroundColor = .blue
         rotated.addSubview(view)
         view.autoPinEdgesToSuperviewEdges()
+        rotated.layer.cornerRadius = view.layer.cornerRadius
         return rotated
     }()
     
     let container: RotatedView = {
         let rotated = RotatedView.newAutoLayout()
+        rotated.backgroundColor = .clear
         let view = TileView.newAutoLayout()
         view.backgroundColor = .red
         rotated.addSubview(view)
+        view.autoPinEdgesToSuperviewEdges()
         return rotated
     }()
     
