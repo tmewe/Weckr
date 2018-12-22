69a288417b11b423b89f5d131530b79a308622fd
Tim Mewe <tim.mewe@me.com>
2018-12-22 13:38:50 +0100

    distance label

--- Weckr/Presentation/Views/Cells/Route/RoutePedestrianTableViewCell.swift
+++ Weckr/Presentation/Views/Cells/Route/RoutePedestrianTableViewCell.swift
@@ -31,18 +31,20 @@ class RoutePedestrianTableViewCell: TileTableViewCell {
                                                              range: nil)
         let words = text.components(separatedBy: " ")
         
-        let directionText = words.prefix(2).joined(separator: " ")
+        let directionText = words.prefix(2).naturalJoined().replacingOccurrences(of: ".", with: "")
         let direction = DirectionInstruction(rawValue: directionText)
         
         let destination = words.dropFirst(3).prefix(1)
 
         infoView.titleLabel.text = direction?.localized.uppercased()
-        infoView.timeLabel.text = "\(maneuver.length/60) MIN"
+        infoView.timeLabel.text = String(format: "%.01f", maneuver.travelTime/60) + " MIN"
         infoView.infoLabel.text = destination.joined().replacingOccurrences(of: ".", with: "")
+        distanceLabel.text = "\(Int(maneuver.length)) meters"
     }
     
     private func addSubviews() {
         tileView.addSubview(infoView)
+        tileView.addSubview(distanceLabel)
     }
     
     private func setupConstraints() {
@@ -52,11 +54,24 @@ class RoutePedestrianTableViewCell: TileTableViewCell {
                                                                  bottom: insets.bottom,
                                                                  right: insets.right),
                                               excludingEdge: .bottom)
-        tileView.autoPinEdge(.bottom, to: .bottom, of: infoView, withOffset: insets.bottom)
+        distanceLabel.autoPinEdge(.top, to: .bottom, of: infoView, withOffset: insets.largeSpacing)
+        distanceLabel.autoPinEdge(.left, to: .left, of: tileView, withOffset: insets.left)
+        distanceLabel.autoPinEdge(.right, to: .right, of: tileView, withOffset: insets.right)
+        tileView.autoPinEdge(.bottom, to: .bottom, of: distanceLabel, withOffset: insets.bottom)
     }
     
     let infoView: BasicInfoView = {
         let view = BasicInfoView.newAutoLayout()
         return view
+    }()
+    
+    let distanceLabel: UILabel = {
+        let label = UILabel.newAutoLayout()
+        label.font = UIFont.systemFont(ofSize: Font.Size.TileCell.title, weight: .semibold)
+        label.textColor = .white
+        label.textAlignment = .left
+        label.alpha = 0.7
+        label.text = "14 meters"
+        return label
     }()
 }
