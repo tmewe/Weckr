1c4da37b5cffbbc9064d8d7c7fcd8039feb2a15a
Tim Mewe <tim.mewe@me.com>
2018-12-23 16:27:22 +0100

    localization in strings file

--- Weckr/Localization/Strings.swift
+++ Weckr/Localization/Strings.swift
@@ -8,6 +8,81 @@
 
 import Foundation
 
-enum Strings {
+struct Strings {
+    struct Walkthrough {
+        
+        struct Landing {
+            static let title = "walkthrough.landing.title".localized
+            static let titleColored = "walkthrough.landing.title.coloredPart".localized
+            static let buttonTitle = "walkthrough.landing.buttonTitle".localized
+        }
+        
+        struct Calendar {
+            static let title = "walkthrough.calendar.title".localized
+            static let titleColored = "walkthrough.calendar.title.coloredPart".localized
+            static let subtitle = "walkthrough.calendar.title2".localized
+            static let subtitleColored = "walkthrough.calendar.title2.coloredPart".localized
+            static let buttonTitle = "walkthrough.calendar.buttonTitle".localized
+        }
+        
+        struct Location {
+            static let title = "walkthrough.location.title".localized
+            static let titleColored = "walkthrough.location.title.coloredPart".localized
+            static let subtitle = "walkthrough.location.title2".localized
+            static let subtitleColored = "walkthrough.location.title2.coloredPart".localized
+            static let buttonTitle = "walkthrough.calendar.buttonTitle".localized
+        }
+        
+        struct Notification {
+            static let title = "walkthrough.notification.title".localized
+            static let titleColored = "walkthrough.notification.title.coloredPart".localized
+            static let subtitle = "walkthrough.notification.title2".localized
+            static let subtitleColored = "walkthrough.notification.title2.coloredPart".localized
+            static let buttonTitle = "walkthrough.notification.buttonTitle".localized
+        }
+        
+        struct Travel {
+            static let title = "walkthrough.travel.title".localized
+            static let titleColored = "walkthrough.travel.title.coloredPart".localized
+            static let subtitle = "walkthrough.travel.title2".localized
+            static let subtitleColored = "walkthrough.travel.title2.coloredPart".localized
+            static let buttonTitle = "walkthrough.travel.buttonTitle".localized
+        }
+        
+        struct MorningRoutine {
+            static let title = "walkthrough.morningroutine.title".localized
+            static let titleColored = "walkthrough.morningroutine.title.coloredPart".localized
+            static let subtitle = "walkthrough.morningroutine.title2".localized
+            static let subtitleColored = "walkthrough.morningroutine.title2.coloredPart".localized
+            static let buttonTitle = "walkthrough.morningroutine.buttonTitle".localized
+        }
+        
+        struct Done {
+            static let title = "walkthrough.done.title".localized
+            static let titleColored = "walkthrough.done.title.coloredPart".localized
+            static let subtitle = "walkthrough.done.title2".localized
+            static let subtitleColored = "walkthrough.done.title2.coloredPart".localized
+            static let buttonTitle = "walkthrough.done.buttonTitle".localized
+        }
+    }
     
+    struct Main {
+        
+    }
+    
+    struct Directions {
+        static let north = "direction.north".localized
+        static let south = "direction.south".localized
+        static let east = "direction.east".localized
+        static let west = "direction.west".localized
+        static let northWest = "direction.northWest".localized
+        static let northEast = "direction.northEast".localized
+        static let southWest = "direction.southWest".localized
+        static let southEast = "direction.southEast".localized
+        static let left = "direction.left".localized
+        static let right = "direction.right".localized
+        static let straight = "direction.straight".localized
+        static let roundabout = "direction.roundabout".localized
+        static let drive = "direction.drive".localized
+    }
 }
