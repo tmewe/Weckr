7d3fc42cf37c556947aa5d84adccd474e2f24fc3
Tim Mewe <tim.mewe@me.com>
2019-01-12 18:15:40 +0100

    implementation alarm scheduler

--- Weckr/Buisness Logic/Services/AlarmSchedulerService.swift
+++ Weckr/Buisness Logic/Services/AlarmSchedulerService.swift
@@ -7,6 +7,8 @@
 //
 
 import Foundation
+import UserNotifications
+import SwiftDate
 
 protocol AlarmSchedulerServiceProtocol {
     func setNotification(with date: Date)
@@ -14,6 +16,25 @@ protocol AlarmSchedulerServiceProtocol {
 
 struct AlarmSchedulerService: AlarmSchedulerServiceProtocol {
     func setNotification(with date: Date) {
+        let notificationCenter = UNUserNotificationCenter.current()
+        notificationCenter.removeAllPendingNotificationRequests()
+
+        let content = UNMutableNotificationContent()
+        content.title = "Wake up"
+        content.body = "Time to get to your first event"
+        content.sound = .default
         
+        let testDate = Date() + 10.seconds
+        let trigger = UNCalendarNotificationTrigger(dateMatching: testDate.dateComponents,
+                                                    repeats: false)
+        let request = UNNotificationRequest(identifier: UUID().uuidString,
+                                            content: content,
+                                            trigger: trigger)
+        
+        notificationCenter.add(request) { (error) in
+            if error != nil {
+                print(error!.localizedDescription)
+            }
+        }
     }
 }
