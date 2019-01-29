e64f4561a770d007a3a7336824ba19fe9d9d3921
Tim Mewe <tim.mewe@me.com>
2019-01-29 11:49:45 +0100

    removes primary key from geo coordinate

--- Weckr/Model/Route/GeoCoordinate.swift
+++ Weckr/Model/Route/GeoCoordinate.swift
@@ -14,29 +14,22 @@ import CoreLocation
 @objcMembers public class GeoCoordinate: Object, Decodable, RealmOptionalType {
     dynamic var latitude: Double = 0.0
     dynamic var longitude: Double = 0.0
-    dynamic var compoundKey: String!
     
     enum CodingKeys: String, CodingKey {
         case latitude = "latitude"
         case longitude = "longitude"
     }
     
-    override public class func primaryKey() -> String? {
-        return "compoundKey"
-    }
-    
     convenience init(lat: Double, long: Double) {
         self.init()
         latitude = lat
         longitude = long
-        compoundKey = "\(latitude),\(longitude)"
     }
     
     convenience init(location: CLLocation) {
         self.init()
         latitude = location.coordinate.latitude
         longitude = location.coordinate.longitude
-        compoundKey = "\(latitude),\(longitude)"
     }
     
     required public init(from decoder: Decoder) throws {
@@ -44,7 +37,6 @@ import CoreLocation
         
         latitude = try container.decode(Double.self, forKey: .latitude)
         longitude = try container.decode(Double.self, forKey: .longitude)
-        compoundKey = "\(latitude),\(longitude)"
 
         super.init()
     }
